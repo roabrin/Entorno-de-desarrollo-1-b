1e4c72cd2569f5005b20b60bf5a2d376032e1595
roabrin <javiermarintrujillo@gmail.com>
2024-03-14 14:02:15 +0100

    Añ Per

---
+++ Personas/src/modelos/Model/Persona.java
@@ -0,0 +1,68 @@
+package modelos.Model;
+
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Persona {
+
+    /**
+     * Default constructor
+     */
+    public Persona() {
+    }
+
+    /**
+     * 
+     */
+    private int DNI;
+
+    /**
+     * 
+     */
+    private String Nombre;
+
+    /**
+     * 
+     */
+    private String Apellido1;
+
+    /**
+     * 
+     */
+    private String Apellido2;
+
+    /**
+     * 
+     */
+    private int Edad = 0;
+
+    /**
+     * @param DNI 
+     * @param Nombre 
+     * @param Apellido1 
+     * @param Apellido2 
+     * @param Edad
+     */
+    public void Persona(int DNI, String Nombre, String Apellido1, String Apellido2, int Edad) {
+        
+    }
+
+    /**
+     * @param DNI
+     */
+    public void SetDNI(int DNI) {
+        
+    }
+
+    /**
+     * @return
+     */
+    public String GetDNI() {
+        
+        return "";
+    }
+
+}
